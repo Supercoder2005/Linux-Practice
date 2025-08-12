44ef510368d6b66ee25a704e979aaadc3f83f7ad
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-08-12 14:20:37 +0530

    substring extraction examples

--- OPPE/variable_manupulation.sh
+++ OPPE/variable_manupulation.sh
@@ -1,7 +1,15 @@
 # Case Conversion in Bash
-var=Banana
+var=AbCd123
 echo $var
 echo ${var^}  # the first letter will be in upper case
 echo ${var,} # the first letter will be in lower case
-echo ${var^^} # the wholw word will be in upper case
-echo ${var,,} # the wholw word will be in lower case
+echo ${var^^} # the whole word will be in upper case
+echo ${var,,} # the whole word will be in lower case
+
+# String length
+echo ${#var}
+
+# Substring extraction
+# the first parameter is for the starting index, the second is for the length
+echo ${var:0:4} # Extracts the first 3 characters
+echo ${var:4:3} # Extracts 3 characters starting from index 4
