4e145cd44c2c9913c27e9d0a50ff51521cb58353
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-08-12 14:11:39 +0530

    Fix typo in comment for upper case conversion in Bash script

--- OPPE/variable_manupulation.sh
+++ OPPE/variable_manupulation.sh
@@ -3,5 +3,5 @@ var=Banana
 echo $var
 echo ${var^}  # the first letter will be in upper case
 echo ${var,} # the first letter will be in lower case
-echo ${var^^} # the wholw word will be in upper 
+echo ${var^^} # the wholw word will be in upper case
 echo ${var,,} # the wholw word will be in lower case
