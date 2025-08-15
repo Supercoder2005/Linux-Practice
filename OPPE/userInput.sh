940574c301382ddd7135af2adf475c422d69a29a
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-08-15 14:39:11 +0530

    Add user input and tokenization functionality to the script

---
+++ OPPE/userInput.sh
@@ -0,0 +1,11 @@
+echo "What is your name?"
+# read the entire line of the input and store in to a variable 
+read name 
+echo "Hello! $name, Welcome to the script!"
+
+
+# tokenization 
+echo "Enter a sentence containing two words:" # Srinjinee Mitra
+read first second # first = Srinjinee , second = Mitra 
+echo "First token: $first"
+echo "Second token: $second"
