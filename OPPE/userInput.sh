cd09151f04a38993a34090fbf431f8f0f3a0996c
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-08-15 15:13:03 +0530

    tokenization example

--- OPPE/userInput.sh
+++ OPPE/userInput.sh
@@ -9,3 +9,8 @@ echo "Enter a sentence containing two words:" # Srinjinee Mitra
 read first second # first = Srinjinee , second = Mitra 
 echo "First token: $first"
 echo "Second token: $second"
+
+# Storing a particular word from a sentence
+echo "Enter a sentence:"
+read a b c variable d
+echo "variable : $variable"
