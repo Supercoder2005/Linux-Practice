71edb349b2ed713c34b4ee21feaa605d993d392f
Srinjinee Mitra <mitrasrinjinee12@gmail.com>
2025-12-20 21:40:30 +0000

    zombie process

---
+++ System Call/ZombieProcess.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int main(){
+    if(fork()==0){
+        printf("\n Child Process Exiting ....\n");
+        exit(0);
+    }
+    else{
+        printf("\n Parent does not receive the child's termination status.\n");
+        printf("\n Parent is sleeping, Child becomes Zombie...\n");
+        sleep(10);
+        printf("\n Parent Wokeup.\n");
+    }
+    return 0;
+}
